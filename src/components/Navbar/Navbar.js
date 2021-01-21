175b7ed030031ebce4805695dc2407bc9181465b
coelhomariana <coelhomariana@outlook.com>
2021-01-21 18:19:40 -0300

    fix(navbar): fixed props
    changed some lines in the props part of the code making it reusable

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Row from 'components/Row'
+import Image from 'components/Image'
+import Text from 'components/Text'
 
 const Navbar = props => {
-  const { children } = props
+  const { title, icon } = props
   return (
-    <Row justifyContent='space-between' backgroundColor='white' width='600px' height='60px' padding='10px' {...props}>
-      {children}
+    <Row justifyContent='space-between' backgroundColor='white' width='600px' height='55px' padding='5px' {...props}>
+      <Image src={icon} size='45px'></Image>
+      <Text margin='10px'>{title}</Text>
     </Row>
   )
 }
--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -2,18 +2,13 @@ import React from 'react'
 
 import Row from 'components/Row'
 import Navbar from 'components/Navbar'
-import Text from 'components/Text'
-import Image from 'components/Image'
 
 import avatar from 'assets/images/avatar.svg'
 
 const Login = () => {
   return (
     <Row justifyContent='center'>
-      <Navbar>
-        <Image src={avatar} alt='Avatar' width='40px' height='40px'></Image>
-        <Text padding='5px'>Título</Text>
-      </Navbar>
+      <Navbar title='Título' icon={avatar}></Navbar>
     </Row>
   )
 }
