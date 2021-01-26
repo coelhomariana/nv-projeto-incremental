f6e9331134d0f91fdca4e2ecd8f5a9b884a4a89a
coelhomariana <coelhomariana@outlook.com>
2021-01-26 17:06:01 -0300

    feat(component modal): added proptypes and state to the modal

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 import Row from 'components/Row'
 import Modal from 'components/Modal'
+import Button from 'components/Button'
+import Column from 'components/Column'
+import Navbar from 'components/Navbar'
+
+import avatar from 'assets/images/avatar.svg'
 
 const Login = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
-    <Row justifyContent='center'>
-      <Modal />
-    </Row>
+    <Column justifyContent='center'>
+      <Navbar icon={avatar} />
+      <Button mt='10px' onClick={() => setIsModalOpen(true)}>
+        Abrir modal
+      </Button>
+      <Modal isModalOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <Row width='100%' height='100px' />
+      </Modal>
+    </Column>
   )
 }
 
