81a157de12a95645e562a4f9bea7d50226a1527b
coelhomariana <coelhomariana@outlook.com>
2021-02-23 18:48:39 -0300

    feat(logout button): a button for the user to logout

--- src/components/DropdownMenu/DropdownMenu.js
+++ src/components/DropdownMenu/DropdownMenu.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 import Card from 'components/Card'
 import Row from 'components/Row'
+import Button from 'components/Button'
 
 const DropdownMenu = ({ isMenuOpen }) => {
   if (!isMenuOpen) return null
+
+  const handleLogout = () => {
+    localStorage.removeItem('username')
+    window.location.reload()
+  }
 
   return (
     <Card
@@ -20,6 +26,9 @@ const DropdownMenu = ({ isMenuOpen }) => {
       <Row height='50px'>Menu3</Row>
       <Row height='50px'>Menu4</Row>
       <Row height='50px'>Menu4</Row>
+      <Button width='100%' onClick={handleLogout}>
+        Logout
+      </Button>
     </Card>
   )
 }
