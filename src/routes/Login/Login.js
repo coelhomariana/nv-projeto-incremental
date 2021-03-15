6fe0da777ddf3f0906dadf479f593b327a4e11ed
coelhomariana <coelhomariana@outlook.com>
2021-03-15 17:27:42 -0300

    feat(signup button): signup button in login page

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -7,8 +7,11 @@ import Column from 'components/Column'
 import Button from 'components/Button'
 import Input from 'components/Input'
 import Row from 'components/Row'
+import Link from 'components/Link'
 
 import { mockLogin } from 'services/auth'
+
+import { unauthenticatedRoutes } from 'helpers/routes'
 
 const Login = () => {
   const { register, handleSubmit, formState, errors } = useForm({ resolver: loginResolver })
@@ -26,31 +29,38 @@ const Login = () => {
   }
 
   return (
-    <Column as='form' onSubmit={handleSubmit(onSubmit)} alignItems='center' justifyContent='center' height='100vh'>
-      <Input
-        name='username'
-        register={register}
-        type='text'
-        label='Usuário'
-        placeholder='Ex.: "usuário"'
-        width='390px'
-        error={errors.username?.message}
-      />
-      <Input
-        name='password'
-        register={register}
-        type='password'
-        label='Senha'
-        placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;'
-        width='390px'
-        error={errors.password?.message}
-      />
-      <Row margin='30px'>
-        <Button type='submit' isLoading={formState.isSubmitting}>
-          Logar
-        </Button>
+    <>
+      <Column as='form' onSubmit={handleSubmit(onSubmit)} alignItems='center' justifyContent='flex-end' height='500px'>
+        <Input
+          name='username'
+          register={register}
+          type='text'
+          label='Usuário'
+          placeholder='Ex.: "usuário"'
+          width='390px'
+          error={errors.username?.message}
+        />
+        <Input
+          name='password'
+          register={register}
+          type='password'
+          label='Senha'
+          placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;'
+          width='390px'
+          error={errors.password?.message}
+        />
+        <Row margin='30px'>
+          <Button type='submit' isLoading={formState.isSubmitting} width='390px'>
+            Logar
+          </Button>
+        </Row>
+      </Column>
+      <Row justifyContent='center'>
+        <Link href={unauthenticatedRoutes[1].path}>
+          <Button width='390px'>Cadastrar</Button>
+        </Link>
       </Row>
-    </Column>
+    </>
   )
 }
 
